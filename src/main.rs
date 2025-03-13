ecee637ac45f33b6b4728669a9df7eaca5ed394f
Yogesh Shahi <yogeshshahi@Ishitas-MacBook-Air.local>
2025-03-13 20:29:46 +0530

    feat: added a sample handler and route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+
+
+
+#[tokio::main]
+async fn main() {
+   let routes_hello = Router::new().route("/hello", get(handler_hello));
+
+   let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+   println!("Running on the server with port 8000");
+   axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+}
+
+
+async fn handler_hello () -> impl IntoResponse{
+    println!("this is from Handler function");
+    return "hello world".into_response();
 }
