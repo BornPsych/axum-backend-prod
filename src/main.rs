26a3f6d4cb5ea98aeef4cdfe16adfa9decfe5687
Yogesh Shahi <yogeshshahi@Ishitas-MacBook-Air.local>
2025-03-14 15:58:33 +0530

    feat: added a quick dev and nested path

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
-use axum::{response::IntoResponse, routing::get, Router};
-
-
+use axum::{
+    Router,
+    extract::{Path, Query},
+    response::IntoResponse,
+    routing::get,
+};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-   let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new().merge(route_hello());
 
-   let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
-   println!("Running on the server with port 8000");
-   axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    println!("Running on the server with port 8000");
+    axum::serve(listener, routes_all.into_make_service())
+        .await
+        .unwrap();
 }
 
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
 
-async fn handler_hello () -> impl IntoResponse{
-    println!("this is from Handler function");
-    return "hello world".into_response();
+fn route_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{user_id}", get(handler_hello2))
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    let name = params.name.as_deref().unwrap();
+    let response = format!("this is hello sting with params {}", name).into_response();
+    println!("this is from Handler function {params:?}");
+    return response;
+}
+
+async fn handler_hello2(Path(user_id): Path<String>) -> impl IntoResponse {
+    let response = format!("this is hello sting with path {user_id}").into_response();
+    println!("this is from Handler function {user_id}");
+    return response;
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
 
 #[tokio::test]
-async fn quick_dev()-> Result<()> {
+async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://127.0.0.1:8000")?;
 
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=yogesh").await?.print().await?;
+    hc.do_get("/hello2/shahi").await?.print().await?;
 
     Ok(())
 }
