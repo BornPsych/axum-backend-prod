19837d7e10409a1e8bbe1ea322c3ec7af8c7890c
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-03-22 23:00:41 +0530

    feat: added Error module to store

--- src/model/store/error.rs
+++ src/model/store/error.rs
@@ -1,33 +1,11 @@
-use crate::model::store;
 use serde::Serialize;
-use serde_with::{DisplayFromStr, serde_as};
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[serde_as]
 #[derive(Debug, Serialize)]
 pub enum Error {
-	EntityNotFound { entity: &'static str, id: i64 },
-	// -- Modules
-	Store(store::Error),
-
-	// -- Externals
-	Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
+	FailToCreatePool(String),
 }
-
-// region:    --- Froms
-impl From<sqlx::Error> for Error {
-	fn from(val: sqlx::Error) -> Self {
-		Self::Sqlx(val)
-	}
-}
-
-impl From<store::Error> for Error {
-	fn from(val: store::Error) -> Self {
-		Self::Store(val)
-	}
-}
-// endregion: --- Froms
 
 // region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
